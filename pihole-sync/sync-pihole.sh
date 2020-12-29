51914e24ef420befcdb98113b48328c137362ae1
Bryan <bzuelly@outlook.com>
2020-12-29 15:27:25 -0500

    added --copy list files

--- pihole-sync/sync-pihole.sh
+++ pihole-sync/sync-pihole.sh
@@ -3,6 +3,7 @@
 clush -n -g pihole2 systemctl stop pihole-FTL
 
 clush -n -g pihole2 --copy /etc/pihole/pihole-FTL*
+clush -n -g pihole2 --copy /etc/pihole/*.list
 clush -n -g pihole2 --copy /etc/pihole/dns-servers.conf
 clush -n -g pihole2 --copy /etc/pihole/setupVars.conf
 
