adf739ce169a641d1b43e2fc0d061651b569bca7
Bryan <bzuelly@outlook.com>
2020-12-29 14:28:55 -0500

    modded to use clush

--- pihole-sync/sync-pihole.sh
+++ pihole-sync/sync-pihole.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-clush -n -w pihole2 systemctl stop pihole-FTL
+clush -n -g pihole2 systemctl stop pihole-FTL
 
-clush -n -w pihole2 --copy /etc/pihole/pihole-FTL*
-clush -n -w pihole2 --copy /etc/pihole/dns-servers.conf
-clush -n -w pihole2 --copy /etc/pihole/setupVars.conf
+clush -n -g pihole2 --copy /etc/pihole/pihole-FTL*
+clush -n -g pihole2 --copy /etc/pihole/dns-servers.conf
+clush -n -g pihole2 --copy /etc/pihole/setupVars.conf
 
-clush -n -w pihole2 systemctl start pihole-FTL
+clush -n -g pihole2 systemctl start pihole-FTL
 
