0ba9505f8ae9376578023a4f7cf09089d5e701b3
codedude <bzuelly@users.noreply.github.com>
2022-02-12 22:05:30 -0500

    Update dns_update-C7.sh

--- sysAdminScripts/dns_update-C7.sh
+++ sysAdminScripts/dns_update-C7.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
 
-interf=ens192
-###ADDR=`/sbin/ifconfig $interf | grep 'inet' | awk '{print $2}' | sed -n 1p`
-ADDR=`/sbin/ip a | grep 'inet' | awk '{print $2}' | sed -n 3p | cut -c 1-13`
-#1STO=`/sbin/ifconfig $interf | grep 'inet' | awk '{print $2}' | sed -n 1p | cut -c 1-3`
-#2NDO=`/sbin/ifconfig $interf | grep 'inet' | awk '{print $2}' | sed -n 1p | cut -c 5-7`
-#3RDO=`/sbin/ifconfig $interf | grep 'inet' | awk '{print $2}' | sed -n 1p | cut -c 9`
-#4THO=`/sbin/ifconfig $interf | grep 'inet' | awk '{print $2}' | sed -n 1p | cut -c 11-13`
-HOST=`hostname`
-DOMAIN=$HOST.zuelly.lcl
-NSHOST=ns1.zuelly.lcl
+ADDR=`/usr/sbin/ip a | grep 'inet' | awk '{print $2}' | sed -n 2p | cut -c 1-14`
+HOST=`hostname -s`
+DOMAIN=$HOST.xlab.lcl
+NSHOST=dns100.xlab.lcl
 nsupdate << EOF
 server $NSHOST
-zone zuelly.lcl
+zone xlab.lcl
 update add $DOMAIN 600 A $ADDR
 send
 EOF
-
-#update add $DOMAIN 600 A $ADDR
-
-
-#update add $HOST 600 PTR $4THO.$3RDO.$2NDO.$1STO.zuely.lcl
-
